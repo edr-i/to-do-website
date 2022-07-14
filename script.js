bc2d2c5901d75875910ae978aecbb657489f9ae7
edr-i <edrikop@gmail.com>
2022-07-14 11:28:51 +0200

    Fully functioning, without design

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var input = document.getElementById("to-do");
 
-
 input.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         addToList(input.value);
@@ -8,15 +7,16 @@ input.addEventListener("keypress", function (event) {
 
         for (let i = 0; i < count; i++) {
             document.getElementById("li-" + i).onmouseover = () => {
-                document.getElementById("li-" + i).style.textDecoration = "line-through";
+                document.getElementById("li-" + i).style.color = "gray";
             };
         }
 
         for (let i = 0; i < count; i++) {
             document.getElementById("li-" + i).onmouseout = () => {
-                document.getElementById("li-" + i).style.textDecoration = "none";
+                document.getElementById("li-" + i).style.color = "black";
             };
         }
+
     }
 });
 
@@ -25,6 +25,9 @@ let count = 0;
 function addToList(text) {
     let element = document.createElement("li");
     element.id = "li-" + count;
+    element.addEventListener('click', (event) => {
+        element.style.textDecoration = "line-through";
+    });
     count++;
 
     element.innerHTML = text;
